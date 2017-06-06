48b6ad5efd2c6a2b9335eeb83f45b24e9e892992
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-06-06 17:06:35 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all=[]
+  def initialize(args)
+    @name = args.fetch("name")
+    @weight = args.fetch("weight")
+    @height = args.fetch("height")
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
 end
